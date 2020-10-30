717d10773a1b1edef1babd6de45fb140c8fdfb6d
bobbylcraig <bobbylcraig@gmail.com>
2020-10-30 21:19:44 +0000

    Deploying to gh-pages from  @ 84d8cd7a42cadad4b6e425a77a4c64f2c6771543 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42a1936f4dbfb8764e6a0d15daa52ea3.js"
+  "/precache-manifest.e16aac61000fe053583526c351ffd7df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
